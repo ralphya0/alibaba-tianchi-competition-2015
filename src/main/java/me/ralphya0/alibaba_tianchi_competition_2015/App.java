5868b5932751f3b5371dfc764ceecf2db1018d34
ralphya0 <ralphya0@foxmail.com>
2015-04-09 09:47:26 +0800

    为天池算法比算新建项目

---
+++ src/main/java/me/ralphya0/alibaba_tianchi_competition_2015/App.java
@@ -0,0 +1,13 @@
+package me.ralphya0.alibaba_tianchi_competition_2015;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
