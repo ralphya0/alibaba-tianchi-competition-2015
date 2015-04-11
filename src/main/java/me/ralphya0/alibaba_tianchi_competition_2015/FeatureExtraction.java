5056ff23284660941af7f6b9da3dbbd859186555
ralphya0 <ralphya0@foxmail.com>
2015-04-11 17:35:59 +0800

    吃饭前提交一把~
    Signed-off-by: ralphya0 <ralphya0@foxmail.com>

--- src/main/java/me/ralphya0/alibaba_tianchi_competition_2015/FeatureExtraction.java
+++ src/main/java/me/ralphya0/alibaba_tianchi_competition_2015/FeatureExtraction.java
@@ -17,6 +17,7 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.PairFlatMapFunction;
 import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.api.java.function.VoidFunction;
 
@@ -40,6 +41,64 @@ class ItemRecord implements Serializable{
     String item_geohash;
     String item_category;
 }
+
+
+//记录计算得到的特征
+class Features implements Serializable{
+    String user_id;
+    String item_id;
+    
+    int tongji_feature1;
+    int tongji_feature2;
+    int tongji_feature3;
+    int tongji_feature4;
+    int tongji_feature5;
+    int tongji_feature6;
+    int tongji_feature7;
+    int tongji_feature8;
+    int tongji_feature9;
+    int tongji_feature10;
+    int tongji_feature11;
+    int tongji_feature12;
+    int tongji_feature13;
+    int tongji_feature14;
+    int tongji_feature15;
+    int tongji_feature16;
+    int tongji_feature17;
+    int tongji_feature18;
+    int tongji_feature19;
+    int tongji_feature20;
+    int tongji_feature21;
+    int tongji_feature22;
+    int tongji_feature23;
+    int tongji_feature24;
+    int tongji_feature25;
+    int tongji_feature26;
+    int tongji_feature27;
+    int tongji_feature28;
+    int tongji_feature29;
+    int tongji_feature30;
+    int tongji_feature31;
+    int tongji_feature32;
+    
+    double bilv_feature1;
+    double bilv_feature2;
+    double bilv_feature3;
+    double bilv_feature4;
+    double bilv_feature5;
+    double bilv_feature6;
+    double bilv_feature7;
+    double bilv_feature8;
+    double bilv_feature9;
+    double bilv_feature10;
+    
+    double zhuanhua_feature1; 
+    double zhuanhua_feature2; 
+    double zhuanhua_feature3; 
+    double zhuanhua_feature4; 
+    double zhuanhua_feature5;
+}
+
 
 //提取用户-商品交互特征
 public class FeatureExtraction {
@@ -215,63 +274,230 @@ public class FeatureExtraction {
         
         //hehe, 忽然发现把records以user_id聚合之后可以直接计算各种特征啦,各种复杂的数据结构可以省略掉 : )
         //计算结果直接写入hdfs中
-        userid_grouped.foreach(new VoidFunction<Tuple2<String,Iterable<InteractionRecord>>>(){
+       JavaPairRDD<Long,Features> features = userid_grouped
+               .flatMapToPair(new PairFlatMapFunction<Tuple2<String,Iterable<InteractionRecord>>,Long,Features>(){
 
-            public void call(Tuple2<String, Iterable<InteractionRecord>> arg0)
-                    throws Exception {
-                String user_id = arg0._1;
-                //采用的数据结构:
-                //Map<String,Map<Long,Map<Long,Map<Integer,Integer>>>>
-                //item_id,split_dates(12-16\12-17\12-18),tongji_hour(1,3,12),action,counter
-                Map<String,Map<Long,Map<Long,Map<Integer,Integer>>>> history = 
-                        new HashMap<String,Map<Long,Map<Long,Map<Integer,Integer>>>>();
-                for(InteractionRecord i : arg0._2){
-                    
-                    if(!history.containsKey(i.item_id)){
-                            for(long s : split_dates_ordered){
-                                Map<Long,Map<Long,Map<Integer,Integer>>> mm3 = new HashMap<Long,Map<Long,Map<Integer,Integer>>>();
-                                
-                                for(long j : tongji_hour){
-                                    Map<Long,Map<Integer,Integer>> mm2 = new HashMap<Long,Map<Integer,Integer>>();
-                                    Map<Integer,Integer> mm1 = new HashMap<Integer,Integer>();
-                                    mm1.put(1, 0);
-                                    mm1.put(2, 0);
-                                    mm1.put(3, 0);
-                                    mm1.put(4, 0);
-                                    mm2.put(j, mm1);
-                                    mm3.put(s, mm2);
-                                }
-                                history.put(i.item_id, mm3);
-                            }
+                public Iterable<Tuple2<Long, Features>> call(
+                        Tuple2<String, Iterable<InteractionRecord>> arg0)
+                        throws Exception {
+                    String user_id = arg0._1;
+                    //采用的数据结构:
+                    Map<String,Map<Long,Map<Long,Map<Integer,Integer>>>> history = 
+                            new HashMap<String,Map<Long,Map<Long,Map<Integer,Integer>>>>();
+                    //另外还需记录各品牌第一次和最后一次访问时间,设置一对特殊的tongji_hour 来表征这两项信息,-1表示第一次,-2表示最后一次
+                    for(InteractionRecord i : arg0._2){
                         
-                    }
-                  //将各条记录插入数据结构的所有合适位置
-                  long inter_hour = i.hour;
-                  int action = Integer.parseInt(i.behavior_type);
-                  String brand = i.item_id;
-                  
-                  //主要关注该记录发生时间与各关键时间点的距离
-                  for(Long key_hour : split_dates_ordered){
-                      //必须将key_hour当天及之后几天的数据排除在外!!!
-                      if(inter_hour < key_hour){
-                        //Map<String,Map<Long,Map<Long,Map<Integer,Integer>>>>
-                          //item_id,split_dates(12-16\12-17\12-18),tongji_hour(1,3,12),action,counter
-                          //long[] tongji_hour = {1, 3, 6, 12, 24, 72, 168, 720};
-                          //范围重叠! 1,1~3,1~6,1~12,1~24,1~72
-                          for(long tezheng_h : tongji_hour){
-                              if(key_hour - inter_hour <= tezheng_h){
-                                  Map<Long,Map<Long,Map<Integer,Integer>>> m1 = history.get(brand);
-                                  Map<Long,Map<Integer,Integer>> m2 = m1.get(key_hour);
-                                  Map<Integer,Integer> m3 = m2.get(tezheng_h);
-                                  m3.put(action, m3.get(action) + 1);
+                        if(!history.containsKey(i.item_id)){
+                                for(long s : split_dates_ordered){
+                                    Map<Long,Map<Long,Map<Integer,Integer>>> mm3 = new HashMap<Long,Map<Long,Map<Integer,Integer>>>();
+                                    
+                                    for(long j : tongji_hour){
+                                        Map<Long,Map<Integer,Integer>> mm2 = new HashMap<Long,Map<Integer,Integer>>();
+                                        Map<Integer,Integer> mm1 = new HashMap<Integer,Integer>();
+                                        mm1.put(1, 0);
+                                        mm1.put(2, 0);
+                                        mm1.put(3, 0);
+                                        mm1.put(4, 0);
+                                        mm2.put(j, mm1);
+                                        mm3.put(s, mm2);
+                                    }
+                                    //here!
+                                   // Map<Long,Map<Integer,Integer>> tmp1 =  
+                                    
+                                    history.put(i.item_id, mm3);
+                                }
+                            
+                        }
+                      //将各条记录插入数据结构的所有合适位置
+                      long inter_hour = i.hour;
+                      int action = Integer.parseInt(i.behavior_type);
+                      String brand = i.item_id;
+                      
+                      //主要关注该记录发生时间与各关键时间点的距离
+                      for(Long split_hour : split_dates_ordered){
+                          //必须将key_hour当天及之后几天的数据排除在外!!!
+                          if(inter_hour < split_hour){
+                            //Map<String,Map<Long,Map<Long,Map<Integer,Integer>>>>
+                              //item_id,split_dates(12-16\12-17\12-18),tongji_hour(1,3,12),action,counter
+                              //long[] tongji_hour = {1, 3, 6, 12, 24, 72, 168, 720};
+                              //范围重叠! 1,1~3,1~6,1~12,1~24,1~72
+                              for(long tezheng_h : tongji_hour){
+                                  if(split_hour - inter_hour <= tezheng_h){
+                                      Map<Long,Map<Long,Map<Integer,Integer>>> m1 = history.get(brand);
+                                      Map<Long,Map<Integer,Integer>> m2 = m1.get(split_hour);
+                                      Map<Integer,Integer> m3 = m2.get(tezheng_h);
+                                      m3.put(action, m3.get(action) + 1);
+                                  }
                               }
                           }
-                          
-                          
                       }
-                  }
-                }
-            }});
+                    }
+                  //Map<String,Map<Long,Map<Long,Map<Integer,Integer>>>>
+                    //item_id,split_dates(12-16\12-17\12-18),tongji_hour(1,3,12),action,counter
+                    //records插入结束,借助history对各个brand计算各项特征
+                    String[] brands = history.keySet().toArray(new String[0]);
+                    if(brands != null){
+                        for(String bid : brands){
+                            Map<Long,Map<Long,Map<Integer,Integer>>> m1 = history.get(bid);
+                            if(m1 != null){
+                                Long[] split_hours = m1.keySet().toArray(new Long[0]);
+                                if(split_hours != null){
+                                    for(long split_h : split_hours){
+                                        Map<Long,Map<Integer,Integer>> m2 = m1.get(split_h);
+                                      
+                                        if(m2 != null){
+                                          //不同的split_h输出独立的Feature
+                                            Features f = new Features();
+                                            f.user_id = user_id;
+                                            f.item_id = bid;
+                                            
+                                            //final static long[] tongji_hour = {1, 3, 6, 12, 24, 72, 168, 720};
+                                            //因为tongji_hour中元素顺序与目标特征之间没有关联,所以只能手动逐个计算
+                                            //先计算统计特征
+                                            if(m2.containsKey(1)){
+                                                Map<Integer,Integer> m3 = m2.get(1);
+                                                int view_ct = m3.get(1);
+                                                int shoucang_ct = m3.get(2);
+                                                int cart_ct = m3.get(3);
+                                                int buy_ct = m3.get(4);
+                                                //开始计算与"之前1小时"有关的统计特征
+                                                f.tongji_feature1 = view_ct;
+                                                f.tongji_feature2 = shoucang_ct;
+                                                f.tongji_feature3 = cart_ct;
+                                                f.tongji_feature4 = buy_ct;
+                                            }
+                                            if(m2.containsKey(6)){
+                                                Map<Integer,Integer> m3 = m2.get(6);
+                                                f.tongji_feature5 = m3.get(1);
+                                                f.tongji_feature6 = m3.get(2);
+                                                f.tongji_feature7 = m3.get(3);
+                                                f.tongji_feature8 = m3.get(4);
+                                            }
+                                            if(m2.containsKey(24)){
+                                                Map<Integer,Integer> m3 = m2.get(24);
+                                                f.tongji_feature9 = m3.get(1);
+                                                f.tongji_feature10 = m3.get(2);
+                                                f.tongji_feature11 = m3.get(3);
+                                                f.tongji_feature12 = m3.get(4);
+                                            }
+                                            if(m2.containsKey(72)){
+                                                Map<Integer,Integer> m3 = m2.get(72);
+                                                f.tongji_feature13 = m3.get(1);
+                                                f.tongji_feature14 = m3.get(2);
+                                                f.tongji_feature15 = m3.get(3);
+                                                f.tongji_feature16 = m3.get(4);
+                                            }
+                                            if(m2.containsKey(168)){
+                                                Map<Integer,Integer> m3 = m2.get(168);
+                                                f.tongji_feature17 = m3.get(1);
+                                                f.tongji_feature18 = m3.get(2);
+                                                f.tongji_feature19 = m3.get(3);
+                                                f.tongji_feature20 = m3.get(4);
+                                            }
+                                            if(m2.containsKey(720)){
+                                                Map<Integer,Integer> m3 = m2.get(720);
+                                                f.tongji_feature21 = m3.get(1);
+                                                f.tongji_feature22 = m3.get(2);
+                                                f.tongji_feature23 = m3.get(3);
+                                                f.tongji_feature24 = m3.get(4);
+                                            }
+                                            
+                                        }
+                                        
+                                    }
+                                }
+                            }
+                            
+                            
+                        }
+                    }
+                    return null;
+                }});
     }
 
 }
+
+  /*      String user_id = arg0._1;
+        //采用的数据结构:
+        
+        Map<String,Map<Long,Map<Long,Map<Integer,Integer>>>> history = 
+                new HashMap<String,Map<Long,Map<Long,Map<Integer,Integer>>>>();
+        for(InteractionRecord i : arg0._2){
+            
+            if(!history.containsKey(i.item_id)){
+                    for(long s : split_dates_ordered){
+                        Map<Long,Map<Long,Map<Integer,Integer>>> mm3 = new HashMap<Long,Map<Long,Map<Integer,Integer>>>();
+                        
+                        for(long j : tongji_hour){
+                            Map<Long,Map<Integer,Integer>> mm2 = new HashMap<Long,Map<Integer,Integer>>();
+                            Map<Integer,Integer> mm1 = new HashMap<Integer,Integer>();
+                            mm1.put(1, 0);
+                            mm1.put(2, 0);
+                            mm1.put(3, 0);
+                            mm1.put(4, 0);
+                            mm2.put(j, mm1);
+                            mm3.put(s, mm2);
+                        }
+                        history.put(i.item_id, mm3);
+                    }
+                
+            }
+          //将各条记录插入数据结构的所有合适位置
+          long inter_hour = i.hour;
+          int action = Integer.parseInt(i.behavior_type);
+          String brand = i.item_id;
+          
+          //主要关注该记录发生时间与各关键时间点的距离
+          for(Long split_hour : split_dates_ordered){
+              //必须将key_hour当天及之后几天的数据排除在外!!!
+              if(inter_hour < split_hour){
+                //Map<String,Map<Long,Map<Long,Map<Integer,Integer>>>>
+                  //item_id,split_dates(12-16\12-17\12-18),tongji_hour(1,3,12),action,counter
+                  //long[] tongji_hour = {1, 3, 6, 12, 24, 72, 168, 720};
+                  //范围重叠! 1,1~3,1~6,1~12,1~24,1~72
+                  for(long tezheng_h : tongji_hour){
+                      if(split_hour - inter_hour <= tezheng_h){
+                          Map<Long,Map<Long,Map<Integer,Integer>>> m1 = history.get(brand);
+                          Map<Long,Map<Integer,Integer>> m2 = m1.get(split_hour);
+                          Map<Integer,Integer> m3 = m2.get(tezheng_h);
+                          m3.put(action, m3.get(action) + 1);
+                      }
+                  }
+              }
+          }
+        }
+      //Map<String,Map<Long,Map<Long,Map<Integer,Integer>>>>
+        //item_id,split_dates(12-16\12-17\12-18),tongji_hour(1,3,12),action,counter
+        //records插入结束,借助history对各个brand计算各项特征
+        String[] brands = history.keySet().toArray(new String[0]);
+        if(brands != null){
+            for(String bid : brands){
+                Map<Long,Map<Long,Map<Integer,Integer>>> m1 = history.get(bid);
+                if(m1 != null){
+                    Long[] split_hours = m1.keySet().toArray(new Long[0]);
+                    if(split_hours != null){
+                        for(long split_h : split_hours){
+                            Map<Long,Map<Integer,Integer>> m2 = m1.get(split_h);
+                            if(m2 != null){
+                                //不同的split_h应该拥有独立的输出文件
+                                Long[] tongji_hours = m2.keySet().toArray(new Long[0]);
+                                if(tongji_hours != null){
+                                    for(long hour : tongji_hours){
+                                        Map<Integer,Integer> m3 = m2.get(hour);
+                                        if(m3 != null){
+                                            //暂且按照顺序计算各项特征
+                                            
+                                        }
+                                    }
+                                }
+                            }
+                            
+                        }
+                    }
+                }
+                
+                
+            }
+        }
+        
+    }}*/
