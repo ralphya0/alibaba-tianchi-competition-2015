af55f544f80bc2245518fa4f0fa7c266320434b7
ralphya0 <ralphya0@foxmail.com>
2015-04-22 14:59:54 +0800

    将libsvm格式输入文件构建功能与GDBT预测功能分离
    Signed-off-by: ralphya0 <ralphya0@foxmail.com>

--- src/main/java/me/ralphya0/alibaba_tianchi_competition_2015/GDBTProcessing.java
+++ src/main/java/me/ralphya0/alibaba_tianchi_competition_2015/GDBTProcessing.java
@@ -1,8 +1,10 @@
 package me.ralphya0.alibaba_tianchi_competition_2015;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -19,6 +21,7 @@ import org.apache.spark.mllib.regression.LabeledPoint;
 import org.apache.spark.mllib.tree.GradientBoostedTrees;
 import org.apache.spark.mllib.tree.configuration.BoostingStrategy;
 import org.apache.spark.mllib.tree.model.GradientBoostedTreesModel;
+import org.apache.spark.mllib.util.MLUtils;
 
 import scala.Tuple2;
 
@@ -26,79 +29,63 @@ public class GDBTProcessing {
 
     public static void main(String[] args) throws Exception {
         
-        //离线测试以及生成提交结果两种功能
+        //构造libsvm输入数据,离线测试以及生成提交结果三种功能
         String action = null;
         String file_path1 = null;
         String file_path2 = null;
         
         String gdbt_a1 = null;
         String gdbt_a2 = null;
+        //用于offline-test
+        String buy_n = null;
         int gdbt_arg1 = 0;
         int gdbt_arg2 = 0;
+        long buy_num = 0;
         if(args != null){
             action = args[0].trim();
             file_path1 = args[1].trim();
             file_path2 = args[2].trim();
             gdbt_a1 = args[3].trim();
             gdbt_a2 = args[4].trim();
-            gdbt_arg1 = Integer.parseInt(gdbt_a1);
-            gdbt_arg2 = Integer.parseInt(gdbt_a2);
+            buy_n = args[5].trim();
+            
         }
         
-        if(action != null && action.equals("offline-test")){
-            if(file_path1 != null && file_path2 != null){
-                //读取并构造训练集
-                List<LabeledPoint> list = new ArrayList<LabeledPoint>();
-                BufferedReader br = new BufferedReader(new FileReader(file_path1));
-                String l = "";
-                while((l = br.readLine()) != null){
-                    String[] al = l.split(",");
-                    if(al != null){
-                        //剔除user_id,item_id和type
-                        double[] features = new double[al.length - 3];
-                        for(int i = 2; i < al.length - 1; i ++){
-                            features[i - 2] = Double.parseDouble(al[i]);
-                            
-                        }
-                        LabeledPoint pt = new LabeledPoint(Double.parseDouble(al[al.length - 1]), Vectors.dense(features));
-                        list.add(pt);
+        if(action != null && action.equals("input-gen")){
+            BufferedReader br = new BufferedReader(new FileReader(file_path1));
+            String l = "";
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file_path2));
+            long buy = 0;
+            while((l = br.readLine()) != null){
+                String[] al = l.split(",");
+                if(al != null){
+                    //剔除user_id,item_id和type,构造libsvm文件
+                    String tmp = al[al.length - 1];
+                    for(int i = 2; i < al.length - 1; i ++){
+                        tmp += " " + (i - 1) + ":" + al[i]; 
+                    }
+                    tmp += "\n";
+                    bw.write(tmp);
+                    if(Double.parseDouble(al[al.length - 1]) == 1){
+                        buy ++;
                     }
                 }
-                br.close();
-                System.out.println("训练集构造完毕");
-                
-                long true_buy_num = 0;
-                
-                //读取并构造预测集
-                List<LabeledPoint> list2 = new ArrayList<LabeledPoint>();
-                BufferedReader br2 = new BufferedReader(new FileReader(file_path2));
-                String l2 = "";
-                while((l2 = br2.readLine()) != null){
-                    String[] al = l2.split(",");
-                    if(al != null){
-                        double[] features = new double[al.length - 3];
-                        for(int i = 2; i < al.length - 1; i ++){
-                            features[i - 2] = Double.parseDouble(al[i]);
-                            
-                        }
-                        //测试集的label仅用于验证模型预测结果的准确性
-                        LabeledPoint pt = new LabeledPoint(Double.parseDouble(al[al.length - 1]), Vectors.dense(features));
-                        list2.add(pt);
-                        if(Double.parseDouble(al[al.length - 1]) == 1){
-                            true_buy_num ++;
-                        }
-                    }
-                }
-                br2.close();
-                
-                System.out.println("测试集构造完成");
-                
-
+            }
+            br.close();
+            bw.close();
+            System.out.println("输入文件构造完毕,已写入 " + file_path2);
+            System.out.println("正样本个数为:" + buy);
+            
+        }
+        else if(action != null && action.equals("offline-test")){
+                gdbt_arg1 = Integer.parseInt(gdbt_a1);
+                gdbt_arg2 = Integer.parseInt(gdbt_a2);
+                buy_num = Long.parseLong(buy_n);
                 SparkConf conf = new SparkConf().setAppName("GDBT-algorithm").setMaster("spark://tianchi-node1:7077");
                 JavaSparkContext sc = new JavaSparkContext(conf);
                 
-                JavaRDD<LabeledPoint> train_data = sc.parallelize(list);
-                JavaRDD<LabeledPoint> test_data = sc.parallelize(list2);
+                JavaRDD<LabeledPoint> train_data = MLUtils.loadLibSVMFile(sc.sc(), file_path1).toJavaRDD();
+                JavaRDD<LabeledPoint> test_data = MLUtils.loadLibSVMFile(sc.sc(), file_path2).toJavaRDD();
                 
                 BoostingStrategy boostingStrategy = BoostingStrategy.defaultParams("Classification");
                 boostingStrategy.setNumIterations(gdbt_arg1);
@@ -144,9 +131,9 @@ public class GDBTProcessing {
                     }
                 }).count();
                 
-                if(true_buy_num != 0 && pre_buy_num != 0){
+                if(buy_num != 0 && pre_buy_num != 0){
                     double precision = correct_num / pre_buy_num;
-                    double recall = correct_num / true_buy_num;
+                    double recall = correct_num / buy_num;
                     double f1 = (2 * precision * recall) / (precision + recall);
                     System.out.println();
                     System.out.println("----------------------------------------------");
@@ -157,7 +144,6 @@ public class GDBTProcessing {
                     System.out.println("F1 = " + f1);
                     
                 }
-            }
         }
         else if(action != null && action.equals("submit")){
            
